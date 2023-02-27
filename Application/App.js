4bc91fa49b08b3eb2c588f288f0fc54b67446002
alperengonull <81569125+alperengonull@users.noreply.github.com>
2023-02-27 19:33:50 +0300

    Update App.js

--- Application/App.js
+++ Application/App.js
@@ -1,12 +1,24 @@
-import { View, Text } from 'react-native'
-import React from 'react'
+import {View, Text, StyleSheet} from 'react-native';
+import React from 'react';
+import { StatusBar } from 'react-native';
 
 const App = () => {
   return (
-    <View>
+    <View  style={styles.container}>
       <Text>App</Text>
+     
+      <StatusBar style="auto" />
     </View>
-  )
-}
+  );
+};
 
-export default App
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default App;
